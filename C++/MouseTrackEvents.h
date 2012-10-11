82261e2821c9b77046303263ed1392b41f34500a
Craig Rowe <craig.rowe@dootrix.com>
2012-10-11 11:57:10 +0100

    Mouse tracking helper class

---
+++ C++/MouseTrackEvents.h
@@ -0,0 +1,14 @@
+#include <Windows.h>
+
+class MouseTrackEvents
+{
+    bool m_bMouseTracking;
+
+public:
+    MouseTrackEvents() : m_bMouseTracking(false)
+    {
+    }
+    
+    void OnMouseMove(HWND hwnd);
+    void Reset(HWND hwnd);
+};
